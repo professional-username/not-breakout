644b8b84275477c61418c64ecba38661eeb3ecb7
professional-username <9hacahn5@anonaddy.me>
2024-07-02 22:24:46 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Game() {
+    return (
+        <div>Game</div>
+    )
+}
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Bouncing Ball Game</h1>
+            <Game/>
+        </div>
+    )
+}
+
+export default App
