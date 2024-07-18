b9798e71d27a2cbae3fcfacd3ba4850ce3f439d0
professional-username <9hacahn5@anonaddy.me>
2024-07-18 16:32:58 +0100

    Improve Game Data display

--- src/components/GameData/GameData.jsx
+++ src/components/GameData/GameData.jsx
@@ -1,18 +1,38 @@
 import "./GameData.scss"
 
+function ColorDisplay({values, label}) {
+    return (
+        <>
+            <div className="colorDisplayLabel">{label}</div>
+            {values.map((value, index) => <div key={index} className={`colorDisplayNumber color-${index}`}>
+                    {value}
+                </div>
+            )}
+        </>
+    )
+}
+
 function GameData({settings, Balls, Blocks}) {
+    // Grab relevant settings
+    const nColors = settings.nColors;
     // Calculate the area of each color
-    const colorAreas = [...Array(settings.nColors).keys()].map((color) => {
+    const colorAreas = [...Array(nColors).keys()].map((color) => {
         const blocksOfColor = Blocks.filter((block) => block.color === color)
         return blocksOfColor.length
     })
 
-    return (
-        <div className="gameData">
-            <p>Game data?</p>
-            <p>{colorAreas.map((area, index) => `${index}: ${area}\n`)}</p>
-        </div>
-    )
+    // Calculate the total speed of each set of balls
+    const ballSpeeds = [...Array(nColors).keys()].map((color) => {
+        const ballsOfColor = Balls.filter((ball) => ball.color === color)
+        const speed = ballsOfColor.reduce((acc, ball) => acc + Math.sqrt(ball.velocity[0] ** 2 + ball.velocity[1] ** 2), 0)
+        // Format speed to 2 decimal places
+        return speed.toFixed(2)
+    })
+
+    return (<div className="gameData" style={{gridTemplateColumns: `2fr repeat(${nColors}, 1fr)`}}>
+        <ColorDisplay values={colorAreas} label="Area"/>
+        <ColorDisplay values={ballSpeeds} label="Speeds"/>
+    </div>)
 }
 
 export default GameData
