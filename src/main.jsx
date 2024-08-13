043981a31c47e07ba14114b2e8d844e201d0a39b
professional-username <9hacahn5@anonaddy.me>
2024-08-13 14:02:15 +0100

    Remove extraneous import of ./index.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '/src/components/App/App'
-import './index.css'
-import './styles/main.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "/src/components/App/App";
+import "./styles/main.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
