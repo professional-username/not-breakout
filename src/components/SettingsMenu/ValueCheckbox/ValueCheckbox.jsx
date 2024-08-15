915f676b27fcbbb03fc4267b9072fedd474edaaf
professional-username <9hacahn5@anonaddy.me>
2024-08-15 20:16:07 +0100

    Update styling of value checkbox

--- src/components/SettingsMenu/ValueCheckbox/ValueCheckbox.jsx
+++ src/components/SettingsMenu/ValueCheckbox/ValueCheckbox.jsx
@@ -8,12 +8,21 @@ function ValueCheckbox({ label, valueName }) {
     return (
         <div className="value-checkbox">
             <div className="value-checkbox__label">{label}</div>
+
             <input
+                id={`checkbox-${valueName}`}
                 className="value-checkbox__checkbox"
                 type="checkbox"
                 checked={value}
                 onChange={() => updateSettings({ [valueName]: !value })}
             />
+
+            <label
+                htmlFor={`checkbox-${valueName}`}
+                className="value-checkbox__pill"
+            >
+                <div className="value-checkbox__pill-knob"></div>
+            </label>
         </div>
     );
 }
