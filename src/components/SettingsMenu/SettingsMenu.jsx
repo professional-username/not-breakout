976f43cdad86a81ec7c2b5c605ae33aa7dfb3f00
professional-username <9hacahn5@anonaddy.me>
2024-08-15 14:55:56 +0100

    Improve settings menu

--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -1,90 +1,119 @@
 import "./SettingsMenu.scss";
-import {SettingsProvider, useSettingsContext} from "../../contexts/SettingsContext.jsx";
+import {
+    SettingsProvider,
+    useSettingsContext,
+} from "../../contexts/SettingsContext.jsx";
 import ReactSlider from "react-slider";
 
-function ValueSlider({label, valueName, increment, min, max}) {
-    const {settings, updateSettings} = useSettingsContext();
+function ValueSlider({ label, valueName, increment, min, max }) {
+    const { settings, updateSettings } = useSettingsContext();
     const value = settings[valueName];
     const handleSliderChange = (value) => {
-        // console.log(value)
-        updateSettings({[valueName]: value});
-    }
+        updateSettings({ [valueName]: value });
+    };
 
     return (
-        <ReactSlider
-            className="settings-menu__slider"
-            marks
-            min={min}
-            max={max}
-            step={increment}
-            value={value}
-            onChange={handleSliderChange}
-        />
-    )
+        <div className="settings-menu__slider-box">
+            <div className="settings-menu__slider-label">{label}</div>
+            <div className="settings-menu__slider-value">{value}</div>
+            <ReactSlider
+                className="settings-menu__slider"
+                marks
+                min={min}
+                max={max}
+                step={increment}
+                value={value}
+                onChange={handleSliderChange}
+            />
+        </div>
+    );
 }
 
-function ValueCheckbox({label, valueName}) {
-    const {settings, updateSettings} = useSettingsContext();
+function ValueCheckbox({ label, valueName }) {
+    const { settings, updateSettings } = useSettingsContext();
     const value = settings[valueName];
 
     return (
-        <label>
+        <div className="settings-menu__checkbox-box">
+            <div className="settings-menu__checkbox-label">{label}</div>
             <input
+                className="settings-menu__checkbox"
                 type="checkbox"
                 checked={value}
-                onChange={() => updateSettings({[valueName]: !value})}
+                onChange={() => updateSettings({ [valueName]: !value })}
             />
-            {label}
-        </label>
-    )
+        </div>
+    );
 }
 
-function IncrementButton({label, valueName, increment = 1, min, max}) {
-    const {settings, updateSettings} = useSettingsContext();
+function IncrementButton({ label, valueName, increment = 1, min, max }) {
+    const { settings, updateSettings } = useSettingsContext();
     const value = settings[valueName];
 
     return (
         <div className={"settings-menu__increment"}>
             <button
-                onClick={() => updateSettings({[valueName]: value - increment})}
+                onClick={() =>
+                    updateSettings({ [valueName]: value - increment })
+                }
             >
                 -
             </button>
-            <div>{label}: {value.toFixed(2)}</div>
+            <div>
+                {label}: {value.toFixed(2)}
+            </div>
             <button
-                onClick={() => updateSettings({[valueName]: value + increment})}
+                onClick={() =>
+                    updateSettings({ [valueName]: value + increment })
+                }
             >
                 +
             </button>
         </div>
-    )
+    );
 }
 
-function ReloadButton({updateSettings}) {
-    const {settings: privateSettings} = useSettingsContext();
+function ReloadButton({ updateSettings }) {
+    const { settings: privateSettings } = useSettingsContext();
     return (
-        <button
-            onClick={() => updateSettings(privateSettings)}
-        >
-            Reload
-        </button>
-    )
+        <button onClick={() => updateSettings(privateSettings)}>Reload</button>
+    );
 }
 
-function SettingsMenu({reloadSettings}) {
+function SettingsMenu({ reloadSettings }) {
     return (
         <div className="settings-menu">
-            {/*Some settings are updated locally, then synced together with a button*/}
             <SettingsProvider>
-                <IncrementButton label="Colors" valueName="nColors"/>
-                <IncrementButton label="Balls" valueName="nBallsPerColor"/>
-                <ReloadButton updateSettings={reloadSettings}/>
+                <ValueSlider
+                    label="Colors"
+                    valueName="nColors"
+                    min={2}
+                    max={6}
+                    increment={1}
+                />
+
+                <ValueSlider
+                    label="Balls"
+                    valueName="nBallsPerColor"
+                    min={1}
+                    max={5}
+                    increment={1}
+                />
+
+                <ValueSlider
+                    label="Gravity"
+                    valueName="gravity"
+                    min={-1}
+                    max={1}
+                    increment={0.1}
+                />
+
+                <ValueCheckbox label="Walls" valueName="enableBorders" />
+
+                <ReloadButton updateSettings={reloadSettings} />
             </SettingsProvider>
-            <IncrementButton label="Gravity" valueName="gravity" increment={0.1}/>
-            <ValueSlider label="Gravity" valueName="gravity" min={-1} max={1} increment={0.1}/>
-            <ValueCheckbox label="Walls" valueName="enableBorders"/>
         </div>
-    )
+    );
 }
 
-export default SettingsMenu
+export default SettingsMenu;
