1112642dc48a0d7b13138a2d6530e0aec20cea9f
professional-username <9hacahn5@anonaddy.me>
2024-08-15 16:54:07 +0100

    Reformat Game.jsx

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,18 +1,16 @@
-import "./Game.scss"
-import {useEffect} from "react"
-import {useSettingsContext} from "/src/contexts/SettingsContext";
-import {useBalls} from "/src/hooks/useBalls";
-import {useBlocks} from "/src/hooks/useBlocks";
+import "./Game.scss";
+import { useEffect } from "react";
+import { useSettingsContext } from "/src/contexts/SettingsContext";
+import { useBalls } from "/src/hooks/useBalls";
+import { useBlocks } from "/src/hooks/useBlocks";
 import Blocks from "/src/components/Blocks/Blocks";
 import Balls from "/src/components/Balls/Balls";
 import Borders from "/src/components/BorderOverlay/BorderOverlay";
 import GameData from "../GameData/GameData.jsx";
 import SettingsMenu from "../SettingsMenu/SettingsMenu.jsx";
 
-
 function Game() {
-
-    const {settings, updateSettings} = useSettingsContext();
+    const { settings, updateSettings } = useSettingsContext();
     const [balls, updateBalls, resetBalls] = useBalls();
     const [blocks, updateBlocks, resetBlocks] = useBlocks();
 
@@ -21,7 +19,7 @@ function Game() {
         updateSettings(newSettings);
         resetBalls(newSettings);
         resetBlocks(newSettings);
-    }
+    };
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -34,18 +32,21 @@ function Game() {
     return (
         <div className="dashboard">
             <div className="sidePanel">
-                <GameData Balls={balls} Blocks={blocks}/>
+                <GameData Balls={balls} Blocks={blocks} />
             </div>
-            <div className="gameBoard" style={{height: settings.envSize, width: settings.envSize}}>
-                <Blocks blocks={blocks}/>
-                <Borders blocks={blocks}/>
-                <Balls balls={balls}/>
+            <div
+                className="gameBoard"
+                style={{ height: settings.envSize, width: settings.envSize }}
+            >
+                <Blocks blocks={blocks} />
+                <Borders blocks={blocks} />
+                <Balls balls={balls} />
             </div>
             <div className="sidePanel">
-                <SettingsMenu reloadSettings={reloadSettings}/>
+                <SettingsMenu reloadSettings={reloadSettings} />
             </div>
         </div>
-    )
+    );
 }
 
-export default Game
+export default Game;
