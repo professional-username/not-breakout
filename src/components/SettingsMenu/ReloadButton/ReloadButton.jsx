db358092c6bf6ed6c415ed27616a096093c217cb
professional-username <9hacahn5@anonaddy.me>
2024-08-16 14:13:38 +0100

    Improve styling of reload button

--- src/components/SettingsMenu/ReloadButton/ReloadButton.jsx
+++ src/components/SettingsMenu/ReloadButton/ReloadButton.jsx
@@ -1,10 +1,22 @@
 import { useSettingsContext } from "/src/contexts/SettingsContext.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRefresh } from "@fortawesome/free-solid-svg-icons";
 import "./ReloadButton.scss";
 
 function ReloadButton({ updateSettings }) {
     const { settings: privateSettings } = useSettingsContext();
     return (
-        <button onClick={() => updateSettings(privateSettings)}>Reload</button>
+        <div className="reload-button">
+            <button
+                className="reload-button__button"
+                onClick={() => updateSettings(privateSettings)}
+            >
+                <FontAwesomeIcon
+                    className="reload-button__icon"
+                    icon={faRefresh}
+                />
+            </button>
+        </div>
     );
 }
 
