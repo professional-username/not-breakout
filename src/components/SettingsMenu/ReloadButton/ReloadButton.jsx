0989bb657f240a01d6f5609daf9bd138a0eacd55
professional-username <9hacahn5@anonaddy.me>
2024-08-16 20:39:06 +0100

    Add popping effect to reload button

--- src/components/SettingsMenu/ReloadButton/ReloadButton.jsx
+++ src/components/SettingsMenu/ReloadButton/ReloadButton.jsx
@@ -1,15 +1,32 @@
 import { useSettingsContext } from "/src/contexts/SettingsContext.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 import "./ReloadButton.scss";
 
 function ReloadButton({ updateSettings }) {
     const { settings: privateSettings } = useSettingsContext();
+
+    // Make the button pop a bit when pressed
+    const [buttonActive, setButtonActive] = useState();
+    const handleClick = () => {
+        // Update the global settings with the private ones
+        updateSettings(privateSettings);
+        // Set and reset the button active state
+        setButtonActive(true);
+        setTimeout(() => {
+            setButtonActive(false);
+        }, 200);
+    };
+
     return (
         <div className="reload-button">
             <button
-                className="reload-button__button"
-                onClick={() => updateSettings(privateSettings)}
+                className={`
+                    reload-button__button
+                    reload-button__button--${buttonActive ? "active" : "inactive"}
+                `}
+                onClick={() => handleClick()}
             >
                 <FontAwesomeIcon
                     className="reload-button__icon"
